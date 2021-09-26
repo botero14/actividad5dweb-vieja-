ae331ab05feaf5b69ad5cd5d75db3f0f417b516d
Bryan Otero <botero@uniguajira.edu.co>
2021-09-25 21:55:23 -0500

    tscconfig

---
+++ app/app.ts
@@ -0,0 +1,13 @@
+"use strict";
+//# sourceMappingURL=app.ts.map
+
+let mostrar = function(nom: string){
+    console.log('Mi nombre es: ${nom}');
+}
+let m = function(){
+    console.log("jjj");
+}
+
+mostrar("Bryan Otero");
+console.log(mostrar);
+m();
---
+++ js/app.js
@@ -0,0 +1,12 @@
+"use strict";
+//# sourceMappingURL=app.ts.map
+let mostrar = function (nom) {
+    console.log('Mi nombre es: ${nom}');
+};
+let m = function () {
+    console.log("jjj");
+};
+mostrar("Bryan Otero");
+console.log(mostrar);
+m();
+//# sourceMappingURL=app.js.map
